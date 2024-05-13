8377bdca00e7998384d272e4f326f4de687dc428
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-05-14 00:15:41 +0300

    Initial commit

---
+++ src/test/java/com/javaguides/demoproject_1/Demoproject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaguides.demoproject_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demoproject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
